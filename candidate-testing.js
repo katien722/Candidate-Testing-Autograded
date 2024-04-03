520add8f750a37637486336d96919b4e0df55bce
katien722 <katien722@gmail.com>
2024-04-03 16:53:33 -0500

    GA 1, Part 2 completed

--- candidate-testing.js
+++ candidate-testing.js
@@ -16,49 +16,66 @@ let candidateAnswer = "";
 
 
 
+
+//if (correctAnswers[i] === candidateAnswers[i])
+
 //TODO: Variables for Part 2
-let questions 
-let correctAnswers
-let candidateAnswers 
+let questions = ["Who was the first American woman in space? ", "True or False: 5 kilometers === 5000 meters? ", "(5+3/2*10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45, what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
+let candidateAnswers = ["", "", "", "", ""];
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-candidateName = input.question("What is your name? ");
-console.log("Hello, " + candidateName + ".");
+  candidateName = input.question("What is your name? ");
+  console.log("Hello, " + candidateName + ".");
 
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = input.question("Who was the first American woman in space? ");
+  //candidateAnswer = input.question("Who was the first American woman in space? ");
 
-
-
+  for (let i = 0; i < questions.length; i++) {
+    candidateAnswers[i] = input.question(`${(i + 1)}) ` + questions[i]);
+console.log(`Your Answer: ${candidateAnswers[i]}\nCorrect Answer: ${correctAnswers[i]}`);
+  }
 }
+  function gradeQuiz(candidateAnswers) {
 
-function gradeQuiz(candidateAnswers) {
-if (correctAnswer === candidateAnswer) {
+      for (let i = 0; i < questions.length; i++) {
+      
+        console.log(`Your Answer: ${candidateAnswers[i]}\nCorrect Answer: ${correctAnswers[i]}`);
+  }
+// for (let i = 0; i === candidateAnswer[i]; i++);
+  // console.log(`Your Answer: ${candidateAnswers[i]}\nCorrect Answer: ${correctAnswers[i]}`); 
 
-  console.log("That is correct.");
-} else {console.log("That is incorrect. Please try again.")
-}
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+
+
+// function gradeQuiz(candidateAnswers) {
+
+// for (let i = 0; i < questions.length; i++) {
+
+//   console.log(`Your Answer: ${candidateAnswers[i]}\nCorrect Answer: ${correctAnswers[i]}`);
+
+
+// TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+
 // I need to add an if/else statement here. If user answer ===Sally Ride, then give "correct" ansewr. IF nElse, try again
+//use template literal --to display candidate answer as it corresponds to correct answer. need new conditional for part 3 bust not here
+
+let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
 
-  let grade;  //TODO 3.2 use this variable to calculate the candidates score.
-
-
-  return grade;
+return grade;
 }
 
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-  
+
   askQuestion();
-  
+
   gradeQuiz(this.candidateAnswers);
 }
 
